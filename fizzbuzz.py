45a5dcaf38f696efa94b013c6d3114828d040933
Raul Piraces <rpiraces@plainconcepts.com>
2020-08-22 21:08:29 +0200

    Put fizzbuzz in a function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,16 @@
-x = 1
+def fizzbuzz(n: int):
+    x = 1
+    while x <= n:
+        if x%3 == 0 and x%5 == 0:
+            print("Fizzbuzz")
+        elif x%3 == 0:
+            print("Fizz")
+        elif x%5 == 0:
+            print("Buzz")
+        else:
+            print(x)
+        x+= 1
 
-while x <= 50:
-    if (x%3 == 0) and (x%5 == 0):
-        print("Fizzbuzz")
-    elif x%3 == 0:
-        print("Fizz")
-    elif x%5 == 0:
-        print("Buzz")
-    else:
-        print(x)
-    x+= 1
+
+n = 50
+fizzbuzz(n)
